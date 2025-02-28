5e3d685c0d203fb69db4819532a04176dbb6be8f
PenguinMaster1624 <PenguinMaster1624@gmail.com>
2025-02-27 22:08:01 -0500

    Added implementation for the help function

--- cogs/misc/help.py
+++ cogs/misc/help.py
@@ -9,7 +9,15 @@ class Help(commands.Cog):
 
     @app_commands.command(name='help', description='Shows what other commands I have')
     async def help(self, interaction: discord.Interaction) -> None:
-        pass
+        embed = discord.Embed(title='Birthday Bot Commands', description='So I\'m a bot related to birthdays. These are the commands I have, and there is a plan that I\'d ping people as it strikes midnight on their birthday.', color=discord.Color.orange())
+        embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.display_avatar)
+        embed.add_field(name='`/insert_birthday <month_num> <day_num>`', value='You can insert your birthday into the database for future pinging on it.', inline=True)
+        embed.add_field(name='`/update_birthday <month_num> <day_num>`', value='Use this in case you accidentally put in the wrong birthday. It circumvents having to delete it and put it back in.', inline=True)
+        embed.add_field(name='`/delete_birthday <month_num> <day_num>`', value='In case you just don\'t want your birthday in the database anymore. Not much else to it.', inline=True)
+        embed.add_field(name='`/help`', value='Displays this. There *might* be more at a later date but it\'s unlikely.', inline=True)
+        embed.set_footer(text='I am still a work in progress')
+
+        await interaction.response.send_message(embed=embed, ephemeral=True)
 
 
 async def setup(bot: commands.Bot):
