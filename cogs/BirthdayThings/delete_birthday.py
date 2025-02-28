bf784cda5a329f40f32d6434161688bb0898beda
PenguinMaster1624 <PenguinMaster1624@gmail.com>
2025-02-27 21:02:03 -0500

    Implemented the delete_birthday function

--- cogs/BirthdayThings/delete_birthday.py
+++ cogs/BirthdayThings/delete_birthday.py
@@ -1,4 +1,5 @@
 from Utils.errors import BirthdayNotFoundError
+from Utils.logger_config import logger
 from discord import app_commands
 from discord.ext import commands
 import discord
@@ -13,7 +14,21 @@ class DeleteBirthday(commands.Cog):
 
     @app_commands.command(name='delete_birthday', description='Strike your birthday from the database')
     async def delete_birthday(self, interaction: discord.Interaction) -> None:
-        pass
+        with sqlite3.connect(self.db_path) as db:
+            cursor = db.cursor()
+            user = list(cursor.execute('SELECT * FROM Birthdays WHERE DiscordID = ?', (interaction.user.id,)))
+            
+            try:
+                if not user:
+                    await interaction.response.send_message('You aren\'t in my database, can\'t delete what doesn\'t exist!', ephemeral = True)
+                    raise BirthdayNotFoundError(interaction.user.name)
+            
+            except BirthdayNotFoundError as error:
+                logger.error(error)
+                return
+
+            cursor.execute('DELETE FROM Birthdays WHERE DiscordID = ?', (interaction.user.id,))
+            await interaction.response.send_message('Successfully struck your birthday from my database')
 
 
 async def setup(bot: commands.Bot):
