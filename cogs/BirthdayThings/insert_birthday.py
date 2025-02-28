a51f95150837e2183e0fa2ef83ba75618503d5ea
PenguinMaster1624 <PenguinMaster1624@gmail.com>
2025-02-27 21:21:43 -0500

    Added implementation for the insert_birthday command

--- cogs/BirthdayThings/insert_birthday.py
+++ cogs/BirthdayThings/insert_birthday.py
@@ -1,9 +1,11 @@
+from Utils.logger_config import logger
+from datetime import date, datetime
 from discord import app_commands
 from discord.ext import commands
-from tzdata import zoneinfo
 import discord
 import sqlite3
 import os
+
 
 class InsertBirthday(commands.Cog):
     def __init__(self, bot: commands.Bot):
@@ -12,7 +14,23 @@ class InsertBirthday(commands.Cog):
 
     @app_commands.command(name='insert_birthday', description='Insert your birthday into the database')
     async def insert_birthday(self, interaction: discord.Interaction, month: int, day: int) -> None:
-        pass
+        try:
+            date(year=datetime.now().year, month=month, day=day)
+        
+        except ValueError as error:
+            logger.error(error)
+            await interaction.response.send_message(error, ephemeral=True)
+            return
+
+        with sqlite3.connect(self.db_path) as db:
+            cursor = db.cursor()
+
+            user = list(cursor.execute('SELECT * FROM Birthdays WHERE DiscordID = ?', (interaction.user.id,)))
+            if user:
+                await interaction.response.send_message('Your birthday already exists in the database. Use the `/update` command if you want to update it')
+                return
+            
+            cursor.execute(f'INSERT INTO Birthdays(?,?,?)', (interaction.user.id, interaction.user.name, f'{month}-{day}'))
 
 
 async def setup(bot: commands.Bot):
